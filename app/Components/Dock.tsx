c5a34bd7abd37fa9ef2a2acb8f6f4a31070518da
nyyakko <nyakonyns@gmail.com>
2026-03-25 18:08:10 -0300

    fix: add missing key to Dock options

--- app/Components/Dock.tsx
+++ app/Components/Dock.tsx
@@ -14,9 +14,9 @@ export default function Dock({ options }: DockProps)
     return (
         <View className="flex flex-row p-4 gap-4 text-xl 2xl:text-2xl rounded-xl bg-red-500 text-orange-800 drop-shadow-2xl border-red-500 border-2">
         {
-            options.map(option => {
+            options.map((option, key) => {
                 return (
-                    <Pressable onPress={option.action} className="flex-1 p-4 bg-orange-400 rounded-xl transition hover:scale-[1.2]">
+                    <Pressable key={key} onPress={option.action} className="flex-1 p-4 bg-orange-400 rounded-xl transition hover:scale-[1.2]">
                         <FontAwesomeIcon icon={option.icon} />
                     </Pressable>
                 )
