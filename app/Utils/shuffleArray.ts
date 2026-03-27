c27e2c4d11ecad3d7396fbc3577fef54b4db8e39
nyyakko <nyakonyns@gmail.com>
2026-03-27 12:11:28 -0300

    feat: add new jokes, update joke of the day logic, update tag system

---
+++ app/Utils/shuffleArray.ts
@@ -0,0 +1,11 @@
+export default function shuffleArray<T>(array: T[]): T[]
+{
+    let _array = array;
+
+    for (let i = _array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [_array[i], _array[j]] = [_array[j], _array[i]];
+    }
+
+    return _array;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -8,28 +8,91 @@ import Dock from "./Components/Dock";
 import FlipCard from "./Components/FlipCard";
 
 import { getRandomSound } from "./Utils/soundEffects";
+import shuffleArray from "./Utils/shuffleArray";
 
 import "./global.css"
 
 const JOKES = [
     {
-        question: "Por que o Platão não consegue passar em Filosofia?",
-        answer: "Porque ele acha que as provas são só sombras na caverna e a nota real está em outro plano.",
-        favorited: false,
+        question: "Como o solipsista terminou o namoro?",
+        answer: "Não é você, sou eu.",
         details: {
-            field: "Metafísica",
-        },
+            fields: ["Solipsismo"]
+        }
     },
-] as const;
+    {
+        question: "Por que o filósofo não atravessou a rua?",
+        answer: "Ele ficou ocupado discutindo a definição de 'atravessar'.",
+        details: {
+            fields: ["Filosofia da Linguagem"]
+        }
+    },
+    {
+        question: "O que dois behavioristas disseram um para o outro depois de comer um pastel?",
+        answer: "Foi bom pra você, foi bom pra mim?",
+        details: {
+            fields: ["Behaviorismo"]
+        }
+    },
+    {
+        question: "O que dizem os filósofos quando não fizeram nada para o jantar?",
+        answer: "Só sei que nada assei.",
+        details: {
+            fields: ["Epistemologia"]
+        }
+    },
+    {
+        question: "O que Marx disse para Engels quando estavam entediados?",
+        answer: "Vamos passar um Trotsky?",
+        details: {
+            fields: ["Marxismo"]
+        }
+    },
+    {
+        question: "Qual o jogo favorito do filósofo?",
+        answer: "É o 'Nietzsche for Speed'.",
+            details: {
+                fields: ["Filosofia de Nietzsche"]
+            }
+    },
+    {
+        question: "Por que Aristóteles tentou ser ator?",
+        answer: "Pra mostrar que tinha potencial.",
+        details: {
+            fields: ["Metafísica", "Potencial e Ato"]
+        }
+    },
+    {
+        question: "Ofereceram cerveja a David Hume, o que ele disse?",
+        answer: "Não tenho o hábito.",
+        details: {
+            fields: ["Empirismo", "Causalidade"]
+        }
+    },
+    {
+        question: "Perguntaram a Tomás de Aquino qual o melhor caminho pro cinema, o que ele disse?",
+        answer: "Há 5 vias e um segredo.",
+        details: {
+            fields: ["Teologia", "Cinco Vias"]
+        }
+    },
+    {
+        question: "Como um positivista reage quando pego no flagra?",
+        answer: "Não Conte pra ninguém, hein!?",
+        details: {
+            fields: ["Positivismo"]
+        }
+    }
+];
 
 type Joke = {
     question: string;
     answer: string;
-    favorited: boolean;
     date: Date;
     details: {
-        field: string;
+        fields: string[];
     }
+    favorited?: boolean;
 };
 
 type JokeOfTheDayProps = {
@@ -57,9 +120,17 @@ function JokeOfTheDay({ joke, onFavorite }: JokeOfTheDayProps)
                     <Text className="text-6xl 2xl:text-6xl">🤔</Text>
                     <Text className="text-white text-center text-3xl 2xl:text-4xl">{joke.question}</Text>
                     <Text className="text-white text-lg 2xl:text-xl">Clique para ver a resposta</Text>
-                    <Text className="rounded-full bg-gradient-to-br from-orange-400 to-orange-600 p-2 absolute text-white bottom-4 left-4 border-orange-500 border-2 text-lg 2xl:text-xl">
-                        { joke.details.field }
-                    </Text>
+                    <View className="flex-row absolute bottom-4 left-4 gap-2">
+                    {
+                        joke.details.fields.map(field => {
+                            return (
+                                <Text className="rounded-full bg-gradient-to-br from-orange-400 to-orange-600 text-white p-1 border-orange-500 border-2 2xl:text-lg">
+                                    { field }
+                                </Text>
+                            );
+                        })
+                    }
+                    </View>
                 </View>,
                 <View className="w-full h-full p-8 bg-gradient-to-br from-orange-400 to-orange-600 rounded-3xl items-center justify-evenly border-orange-500 border-2 select-none">
                     { joke.favorited &&
@@ -70,9 +141,17 @@ function JokeOfTheDay({ joke, onFavorite }: JokeOfTheDayProps)
                     <Text className="text-6xl 2xl:text-6xl">😂</Text>
                     <Text className="text-white text-center text-3xl 2xl:text-4xl">{joke.answer}</Text>
                     <Text className="text-white text-lg 2xl:text-xl">Clique para voltar</Text>
-                    <Text className="rounded-full bg-gradient-to-br from-red-400 to-red-600 p-2 absolute text-white bottom-4 left-4 border-red-500 border-2 text-lg 2xl:text-xl">
-                        { joke.details.field }
-                    </Text>
+                    <View className="flex-row absolute bottom-4 left-4 gap-2">
+                    {
+                        joke.details.fields.map(field => {
+                            return (
+                                <Text className="rounded-full bg-gradient-to-br from-red-400 to-red-600 text-white p-1 border-red-500 border-2 2xl:text-lg">
+                                    { field }
+                                </Text>
+                            );
+                        })
+                    }
+                    </View>
                 </View>
             ]}
             </FlipCard>
@@ -110,9 +189,17 @@ function PreviousJokes({ jokes }: PreviousJokesProps)
                         <Text className="text-6xl 2xl:text-6xl">🤔</Text>
                         <Text className="text-white text-center text-3xl 2xl:text-4xl">{joke.question}</Text>
                         <Text className="text-white text-lg 2xl:text-xl">Clique para ver a resposta</Text>
-                        <Text className="rounded-full bg-gradient-to-br from-orange-400 to-orange-600 p-2 absolute text-white bottom-4 left-4 border-orange-500 border-2 text-lg 2xl:text-xl">
-                            { joke.details.field }
-                        </Text>
+                        <View className="flex-row absolute bottom-4 left-4 gap-2">
+                        {
+                            joke.details.fields.map(field => {
+                                return (
+                                    <Text className="rounded-full bg-gradient-to-br from-orange-400 to-orange-600 text-white p-1 border-orange-500 border-2 2xl:text-lg">
+                                        { field }
+                                    </Text>
+                                );
+                            })
+                        }
+                        </View>
                     </View>,
                     <View className="h-full p-8 bg-gradient-to-br from-orange-400 to-orange-600 rounded-3xl items-center justify-evenly border-orange-500 border-2 select-none">
                         { joke.favorited &&
@@ -123,9 +210,17 @@ function PreviousJokes({ jokes }: PreviousJokesProps)
                         <Text className="text-6xl 2xl:text-6xl">😂</Text>
                         <Text className="text-white text-center text-3xl 2xl:text-4xl">{joke.answer}</Text>
                         <Text className="text-white text-lg 2xl:text-xl">Clique para voltar</Text>
-                        <Text className="rounded-full bg-gradient-to-br from-red-400 to-red-600 p-2 absolute text-white bottom-4 left-4 border-red-500 border-2 text-lg 2xl:text-xl">
-                            { joke.details.field }
-                        </Text>
+                        <View className="flex-row absolute bottom-4 left-4 gap-2">
+                        {
+                            joke.details.fields.map(field => {
+                                return (
+                                    <Text className="rounded-full bg-gradient-to-br from-red-400 to-red-600 text-white p-1 border-red-500 border-2 2xl:text-lg">
+                                        { field }
+                                    </Text>
+                                );
+                            })
+                        }
+                        </View>
                     </View>
                 ]}
                 </FlipCard>
@@ -147,7 +242,7 @@ export default function App()
 {
     const [currentWindowIndex, setWindowIndex] = useState(0);
 
-    const [jokes, setJokes] = useState(JOKES.map((joke, i): Joke => {
+    const [jokes, setJokes] = useState(shuffleArray(JOKES).map((joke, i): Joke => {
         let date = new Date();
         date.setDate(date.getDate() - i);
         return { ...joke, date };
